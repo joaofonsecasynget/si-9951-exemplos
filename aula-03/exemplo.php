2f29a52cf0b8d57595df0135fc7221928bb3c04d
João Fonseca <177309629+joaofonsecasynget@users.noreply.github.com>
2026-09-23 13:12:17 +0100

    exemplo da aula 03 t1

---
+++ aula-03/exemplo.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        // Isto é um comentário
+        echo "Isto vem do servidor.";
+        /* echo "Isto vem do servidor.";
+        echo "Isto vem do servidor.";
+        echo "Isto vem do servidor."; */
+        ?>
+    </p>
+    <p>
+        <?php echo date("d/m/Y"); ?>
+    </p>
+</body>
+
+</html>
---
+++ aula-03/ola.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Olá PHP</title>
+</head>
+
+<body>
+    <h1>
+        <?php
+        echo ("O meu primeiro script PHP");
+        ?>
+    </h1>
+    <p>
+        <?php
+        echo ("Isto vem do servidor.");
+        ?>
+    </p>
+</body>
+
+</html>
